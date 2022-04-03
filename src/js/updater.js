b953a3c9df9d017133fb16417d7ea33b8105792d
Sergey Pimenov <sergey@pimenov.com.ua>
2022-04-03 17:53:09 +0300

    added statuses

--- src/js/updater.js
+++ src/js/updater.js
@@ -4,13 +4,28 @@ import {n2f} from "./utils";
 
 export const updateApiData = () => {
     const ledger = data.api
+    const apiStatus = $("#api_status")
 
-    if (!ledger) return
+    if (ledger) {
+        $("#chain_id").text(ledger.chain_id)
+        $("#epoch").text(ledger.epoch)
+        $("#ledger_version").text(ledger.ledger_version)
+        $("#ledger_timestamp").text(datetime(ledger.ledger_timestamp / 1000).format(DATE_TIME_FORMAT))
+    } else {
+        $("#chain_id").text(0)
+        $("#epoch").text(0)
+        $("#ledger_version").text(0)
+        $("#ledger_timestamp").text(DATE_TIME_FORMAT)
+    }
 
-    $("#chain_id").text(ledger.chain_id)
-    $("#epoch").text(ledger.epoch)
-    $("#ledger_version").text(ledger.ledger_version)
-    $("#ledger_timestamp").text(datetime(ledger.ledger_timestamp / 1000).format(DATE_TIME_FORMAT))
+    apiStatus.parent().removeClassBy("bg-")
+    if (ledger && ledger.chain_id) {
+        apiStatus.parent().addClass("bg-green")
+        apiStatus.text("CONNECTED")
+    } else {
+        apiStatus.parent().addClass("bg-red")
+        apiStatus.text("NOT CONNECTED")
+    }
 }
 
 export const updateHealthData = () => {
@@ -23,6 +38,7 @@ export const updateHealthData = () => {
 
 export const updateMetricData = (d) => {
     let metric
+    const status = !!d
 
     if (!d) {
         metric = {
@@ -96,6 +112,39 @@ export const updateMetricData = (d) => {
 
     for (let o in metric) {
         $(`#${o}`).text(n2f(metric[o]))
+    }
+
+    const syncStatus = $("#sync_status")
+
+    syncStatus.parent().removeClassBy("bg-")
+    if (+metric.sync_synced > 0 && Math.abs(metric.sync_synced - metric.sync_target) <= 2 ) {
+        syncStatus.parent().addClass("bg-green")
+        syncStatus.text("SYNCED")
+    } else {
+        syncStatus.parent().addClass("bg-red")
+        syncStatus.text("NOT SYNCED")
+    }
+
+    const peerStatus = $("#peer_status")
+
+    peerStatus.parent().removeClassBy("bg-")
+    if (+metric.connections_outbound > 0 ) {
+        peerStatus.parent().addClass("bg-green")
+        peerStatus.text("OK")
+    } else {
+        peerStatus.parent().addClass("bg-red")
+        peerStatus.text("NO PEERS")
+    }
+
+    const metricStatus = $("#metric_status")
+
+    metricStatus.parent().removeClassBy("bg-")
+    if (status) {
+        metricStatus.parent().addClass("bg-green")
+        metricStatus.text("CONNECTED")
+    } else {
+        metricStatus.parent().addClass("bg-red")
+        metricStatus.text("NOT CONNECTED")
     }
 }
 
